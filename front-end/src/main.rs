73a2a9bb94b2551bed73006a5311f68da82d46f2
Michael Perry Goodman <mpg@demonlord.org>
2022-08-28 17:45:09 -0500

    Small update

--- front-end/src/main.rs
+++ front-end/src/main.rs
@@ -9,6 +9,47 @@ extern "C" {
     pub async fn hello(name: String) -> Result<JsValue, JsValue>;
 }
 
+enum Msg {
+    AddOne,
+}
+
+struct App {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            value: 0,
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
+        html! {
+            <div>
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
+
 #[function_component(App)]
 pub fn app() -> Html {
     let welcome = use_state_eq(|| "".to_string());
